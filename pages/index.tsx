0d16a7a97b816b5a26a77a7b079d6220e292bda9
Ejaaa <muhammadezhasyafaat08@gmail.com>
2022-10-06 15:23:25 +0700

    fixing

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ const Home = () => {
               />
               </li>
               <li>
-                <a className="bg-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8 font-lexend" target="_blank" href="../resume-ezhasyafaat.pdf">
+                <a className="bg-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8 font-lexend" target="_blank" href="#">
                   Resume
                 </a>
               </li>
