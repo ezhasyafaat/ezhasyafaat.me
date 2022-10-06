2a7652b2fa3c9bd0303bdedab1e5d38b3cad7e23
Ejaaa <muhammadezhasyafaat08@gmail.com>
2022-10-06 15:17:14 +0700

    ☕Init Commit

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link 
+                    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" 
+                    rel="stylesheet"
+                />
+                <link 
+                    href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" 
+                    rel="stylesheet" 
+                />
+            </Head>
+            <body className='font-poppins'>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}   
